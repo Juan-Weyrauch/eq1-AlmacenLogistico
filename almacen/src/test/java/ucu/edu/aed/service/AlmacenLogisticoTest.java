3565cf151185e653965de8476669e4b06050c7da
Dolphin <ignacioalonsosclavo@gmail.com>
2026-08-23 19:20:24 -0300

    test

--- almacen/src/test/java/ucu/edu/aed/service/AlmacenLogisticoTest.java
+++ almacen/src/test/java/ucu/edu/aed/service/AlmacenLogisticoTest.java
@@ -2,8 +2,6 @@ package ucu.edu.aed.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ucu.edu.aed.model.Inventario;
-import ucu.edu.aed.model.ItemInventario;
 import ucu.edu.aed.model.Producto;
 import ucu.edu.aed.model.TerminalCarga;
 
@@ -14,20 +12,48 @@ class AlmacenLogisticoTest {
     private TerminalCarga terminal1;
     private TerminalCarga terminal2;
 
+    private Producto productoPrueba;
+
     @BeforeEach
     void setUp() {
         almacen = new AlmacenLogistico();
         terminal1 = new TerminalCarga(1);
         terminal2 = new TerminalCarga(2);
+        productoPrueba = new Producto("1","prueba","es una prueba");
 
     }
 
     @Test
     void registrarTerminal() {
+        almacen.registrarTerminal(terminal1);
+        assertTrue(almacen.getTerminales().contiene(terminal1));
     }
 
     @Test
+    void registrarTerminalDuplicada() {
+        almacen.registrarTerminal(terminal1);
+        TerminalCarga duplicada = new TerminalCarga(1);
+        assertThrows(IllegalArgumentException.class, () -> almacen.registrarTerminal(duplicada));
+    }
+
+    @Test
+    void buscarTerminalLibre() {
+        almacen.registrarTerminal(terminal1);
+        assertEquals(terminal1, almacen.buscarTerminalLibre());
+    }
+
+
+    @Test
+    void buscarTerminalLibreSinTermialLibre() {
+        assertNull(almacen.buscarTerminalLibre());
+    }
+
+
+
+    @Test
     void registrarProducto() {
+        almacen.registrarProducto(productoPrueba, 10);
+        assertEquals(10, almacen.getInventario().obtenerStock(productoPrueba.getCodigo()));
     }
 
     @Test
@@ -55,10 +81,6 @@ class AlmacenLogisticoTest {
     }
 
     @Test
-    void buscarTerminalLibre() {
-    }
-
-    @Test
     void cantidadTotalUnidadesEnInventario() {
     }
 
@@ -73,6 +95,7 @@ class AlmacenLogisticoTest {
 
         assertEquals(almacen.getInventario().buscarItem("000"), unProducto.getCodigo());
     }
+
 
     @Test
     void getTerminales() {
